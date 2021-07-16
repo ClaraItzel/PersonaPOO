0750df43197f3f119046c8a463efba2fd00be849
ClaraItzel <81541685+ClaraItzel@users.noreply.github.com>
2021-07-15 21:24:15 -0500

    Ejercicio java

---
+++ PersonaPOO/src/Persona.java
@@ -0,0 +1,11 @@
+
+public class Persona {
+String name="";
+int age=0;
+	public String saludar() {
+		return "Hola soy " + this.name;
+	}
+	public String Edad() {
+		return "Mi Edad es " + this.age;
+	}
+}
---
+++ PersonaPOO/src/Principal.java
@@ -0,0 +1,14 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona clara = new Persona();
+		clara.name = "Clara Itzel";
+		clara.age= 21;
+		System.out.println(clara.saludar());
+		System.out.println(clara.Edad());
+		
+	}
+
+}
